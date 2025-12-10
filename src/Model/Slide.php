f7b5fe1c248458a511c0315f60a04fe83ebf67e5
John Learn <jflearn@logicbrush.com>
2025-12-10 14:37:29 -0500

    formatting.

--- src/Model/Slide.php
+++ src/Model/Slide.php
@@ -1,10 +1,4 @@
 <?php
-/**
- * src/Model/Slide.php
- *
- * @package default
- */
-
 
 namespace Logicbrush\HeroContent\Model;
 
@@ -27,78 +21,70 @@ use SilverStripe\Versioned\Versioned;
  * @method Page Page()
  * @mixin Versioned
  */
-class Slide extends DataObject
-{
+class Slide extends DataObject {
+
+	private static $singular_name = 'Slide';
+	private static $plural_name = 'Slides';
+	private static $table_name = 'Slide';
+
+	private static $default_sort = 'SortOrder ASC';
+
+	private static $extensions = [
+		Versioned::class,
+	];
+
+	private static $summary_fields = [
+		'Image.CMSThumbnail',
+		'Content',
+	];
+
+	private static $field_labels = [
+		'Image.CMSThumbnail' => 'Image',
+	];
+
+	private static $has_one = [
+		'Image' => Image::class,
+		'Page' => Page::class,
+	];
+
+	private static $owns = [
+		'Image',
+	];
+
+	private static $db = [
+		'Content' => 'HTMLText',
+		'AdditionalHTML' => 'HTMLText',
+		'SortOrder' => 'Int',
+	];
 
 	/**
 	 *
 	 * @Metrics( crap = 1 )
-	 * @return unknown
 	 */
 	#[Override]
+	public function getCMSFields() {
+		$fields = parent::getCMSFields();
 
-/**
- *
- * @Metrics( crap = 1 )
- */
-public function getCMSFields() {
-	$fields = parent::getCMSFields();
+		$fields->removeByName('SortOrder');
+		$fields->removeByName('PageID');
 
-	$fields->removeByName( 'SortOrder' );
-	$fields->removeByName( 'PageID' );
+		$fields->addFieldToTab(
+			'Root.Main',
+			$field = UploadField::create('Image', 'Image')
+		);
+		$field->setFolderName('slides');
+		// $field->getValidator()->setAllowedMaxFileSize( 10485760 );
 
-	$fields->addFieldToTab(
-		'Root.Main',
-		$field = UploadField::create( 'Image', 'Image' )
-	);
-	$field->setFolderName( 'slides' );
-	// $field->getValidator()->setAllowedMaxFileSize( 10485760 );
+		$fields->addFieldToTab(
+			'Root.Advanced',
+			$field = TextareaField::create(
+				'AdditionalHTML',
+				'Additional HTML'
+			)
+		);
+		$field->setRightTitle('You can add additional HTML code to the slide her.');
+		$field->setRows(10);
 
-	$fields->addFieldToTab(
-		'Root.Advanced',
-		$field = TextareaField::create(
-			'AdditionalHTML',
-			'Additional HTML'
-		)
-	);
-	$field->setRightTitle( 'You can add additional HTML code to the slide her.' );
-	$field->setRows( 10 );
-
-	return $fields;
-}
-
-
-private static $singular_name = 'Slide';
-private static $plural_name = 'Slides';
-private static $table_name = 'Slide';
-
-private static $default_sort = 'SortOrder ASC';
-
-private static $extensions = [
-	Versioned::class,
-];
-
-private static $summary_fields = [
-	'Image.CMSThumbnail',
-	'Content',
-];
-
-private static $field_labels = [
-	'Image.CMSThumbnail' => 'Image',
-];
-
-private static $has_one = [
-	'Image' => Image::class,
-	'Page' => Page::class,
-];
-
-private static $owns = [
-	'Image',
-];
-
-private static $db = [
-	'Content' => 'HTMLText',
-	'AdditionalHTML' => 'HTMLText',
-	'SortOrder' => 'Int',
-];
+		return $fields;
+	}
 }
